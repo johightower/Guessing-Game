b45ecb9364f3689d0f19ce817d5803498a89dcad
Joey <joeyhightower@Joeys-MBP.bak.rr.com>
2022-10-13 14:04:20 -0700

    Guessing Game 3

--- index.js
+++ index.js
@@ -20,23 +20,39 @@ guessingGame.style.justifyContent = "center"
 
 const bigBox = document.createElement("div")
 bigBox.style.border = "1px solid black"
-bigBox.style.backgroundColor = ""
-bigBox.style.width = "75%"
+bigBox.style.backgroundColor = "#014737"
+bigBox.style.width = "85%"
 bigBox.style.textAlign = "center"
 
 const title = document.createElement("h1")
 title.textContent = "THE GUESSING GAME"
+title.style.color = "ivory"
 
 const direction = document.createElement("h4")
 direction.textContent = "Guess a number between 1-100!"
+direction.style.color = "ivory"
 
 const form = document.createElement("form")
 
 let input = document.createElement("input")
-input.placeholder = "Type guess here!"
+input.placeholder = "?"
+input.style.color = "ivory"
+input.style.fontSize = "75px"
+input.style.textAlign = "center"
+input.style.borderRadius = "50%"
+input.style.border = "1px dotted ivory"
+input.style.height = "120px"
+input.style.width = "120px"
+input.style.backgroundColor = "#E69b63"
 
 const go = document.createElement("button")
 go.textContent = "Go!"
+go.style.borderRadius = "50%"
+go.style.color = "ivory"
+go.style.backgroundColor = "#a1491c"
+go.style.border = "1px dotted ivory"
+go.style.height = "50px"
+go.style.width = "50px"
 
 const lowerSection = document.createElement("ul")
 lowerSection.style.listStyle = "none"
@@ -44,38 +60,86 @@ lowerSection.style.display = "flex"
 lowerSection.style.alignItems = "center"
 lowerSection.style.justifyContent = "space-around"
 
+
+
+
 let guess1 = document.createElement("li")
-guess1.style.border = "1px solid black"
-guess1.style.height = "15px"
-guess1.style.width = "25px"
-guess1.placeholder = "-"
+guess1.style.border = "1px solid ivory"
+guess1.style.color = "ivory"
+guess1.style.fontSize = "30px"
+guess1.style.height = "30px"
+guess1.style.width = "40px"
+guess1.style.backgroundColor = "#6F3637"
 let guess2 = document.createElement("li")
-guess2.style.border = "1px solid black"
-guess2.style.height = "15px"
-guess2.style.width = "25px"
-guess2.placeholder = "-"
+guess2.style.border = "1px solid ivory"
+guess2.style.color = "ivory"
+guess2.style.fontSize = "30px"
+guess2.style.height = "30px"
+guess2.style.width = "40px"
+guess2.style.backgroundColor = "#B35340"
 let guess3 = document.createElement("li")
-guess3.style.border = "1px solid black"
-guess3.style.height = "15px"
-guess3.style.width = "25px"
-guess3.placeholder = "-"
+guess3.style.border = "1px solid ivory"
+guess3.style.color = "ivory"
+guess3.style.fontSize = "30px"
+guess3.style.height = "30px"
+guess3.style.width = "40px"
+guess3.style.backgroundColor = "#233342"
 let guess4 = document.createElement("li")
-guess4.style.border = "1px solid black"
-guess4.style.height = "15px"
-guess4.style.width = "25px"
-guess4.placeholder = "-"
+guess4.style.border = "1px solid ivory"
+guess4.style.color = "ivory"
+guess4.style.fontSize = "30px"
+guess4.style.height = "30px"
+guess4.style.width = "40px"
+guess4.style.backgroundColor = "#4B2B31"
 let guess5 = document.createElement("li")
-guess5.style.border = "1px solid black"
-guess5.style.height = "15px"
-guess5.style.width = "25px"
-guess5.placeholder = "-"
+guess5.style.border = "1px solid ivory"
+guess5.style.color = "ivory"
+guess5.style.fontSize = "30px"
+guess5.style.height = "30px"
+guess5.style.width = "40px"
+guess5.style.backgroundColor = "#CE8054"
+
+
 
 const footer = document.createElement("footer")
+footer.style.padding = "10px"
+footer.style.display = "flex"
+footer.style.justifyContent = "space-around"
 
 const reset = document.createElement("button")
 reset.textContent = "Reset?"
+reset.style.color = "ivory"
+reset.style.backgroundColor = "#611d06"
+reset.style.width = "10em"
+reset.style.boxShadow = "-3px 3px 3px 0px ivory"
+
 const hint = document.createElement("button")
 hint.textContent = "Hint?"
+hint.style.backgroundColor = "#300804"
+hint.style.color = "ivory"
+hint.style.width = "10em"
+hint.style.boxShadow = "-3px 3px 3px 0px ivory"
+
+
+
+let numberGuess = Math.floor(Math.random() * 100 + 1)
+let count = 0
+go.addEventListener("click", function(event){
+    event.preventDefault();
+    guess1.textContent = input.value;
+
+ if(input.textContent == numberGuess){
+    alert("You did it! I'm proud of you")
+ } else if (input.textContent < numberGuess && count < 5){ 
+    count++
+    alert('↑')
+ } else if (input.textContent > numberGuess && count <5){ 
+    count++ 
+    alert('↓')
+ } else (
+    alert("Awww, not quite. But try again!")
+ )
+ })
 
 guessingGame.append(bigBox)
 bigBox.append(title, direction, form, lowerSection, footer)
